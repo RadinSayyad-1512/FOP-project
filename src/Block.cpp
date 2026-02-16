9fc51fa51533270638267a537be04712508495e5
Radin <raden.sayyad.1512@gmail.com>
2026-02-16 22:17:35 +0330

    Block Color Fixed

--- src/Block.cpp
+++ src/Block.cpp
@@ -5,30 +5,42 @@ Block::Block(BlockType t, std::string txt, float startX, float startY) {
     text = txt;
     x = startX;
     y = startY;
-    w = 160.0f;
+    w = 170.0f;
     h = 45.0f;
-
-    if (type == BlockType::Events)
-        color = {255,191,0,255};
-    else if (type == BlockType::Motion)
-        color = {76,151,255,255};
-    else if (type == BlockType::Looks)
-        color = {153,102,255,255};
-    else if (type == BlockType::Control)
-        color = {255,171,25,255};
-    else
-        color = {150,150,150,255};
 }
 
 void Block::render(SDL_Renderer* renderer) {
+    SDL_Rect rect = { (int)x, (int)y, (int)w, (int)h };
 
-    SDL_Rect rect = {(int)x, (int)y, (int)w, (int)h};
-
-    SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, 255);
+    switch (type) {
+        case BlockType::Motion:
+            SDL_SetRenderDrawColor(renderer, 76, 151, 255, 255);
+            break;
+        case BlockType::Looks:
+            SDL_SetRenderDrawColor(renderer, 153, 102, 255, 255);
+            break;
+        case BlockType::Events:
+            SDL_SetRenderDrawColor(renderer, 255, 191, 0, 255);
+            break;
+        case BlockType::Control:
+            SDL_SetRenderDrawColor(renderer, 255, 171, 25, 255);
+            break;
+        case BlockType::Sensing:
+            SDL_SetRenderDrawColor(renderer, 76, 177, 214, 255);
+            break;
+        case BlockType::Operators:
+            SDL_SetRenderDrawColor(renderer, 64, 191, 74, 255);
+            break;
+        case BlockType::Variables:
+            SDL_SetRenderDrawColor(renderer, 255, 140, 26, 255);
+            break;
+        default:
+            SDL_SetRenderDrawColor(renderer, 160, 160, 160, 255);
+            break;
+    }
 
     SDL_RenderFillRect(renderer, &rect);
 
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderFillRect(renderer, &rect);
-
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 100);
+    SDL_RenderDrawRect(renderer, &rect);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,37 @@
 #define SDL_MAIN_HANDLED
 #include <SDL2/SDL.h>
 #include <vector>
-#include <iostream>
 #include "../include/Block.h"
 
 int main(int argc, char* argv[]) {
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        std::cerr << "SDL Initialization Failed: " << SDL_GetError() << std::endl;
-        return -1;
-    }
-
-    SDL_Window* window = SDL_CreateWindow("FOP Project", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-                                            1024, 768, SDL_WINDOW_SHOWN);
-    if (!window)
-        return -1;
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window* window = SDL_CreateWindow("Sharif Scratch - Phase 1", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1024, 768, SDL_WINDOW_SHOWN);
 
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
 
     std::vector<Block> myBlocks;
-
-    myBlocks.push_back(Block(BlockType::Events, "When Flag Clicked", 50, 50));
-    myBlocks.push_back(Block(BlockType::Motion, "Move 10 Steps", 50, 100));
-    myBlocks.push_back(Block(BlockType::Looks, "Say Hello", 50, 150));
-    myBlocks.push_back(Block(BlockType::Control, "Forever", 50, 200));
+    myBlocks.push_back(Block(BlockType::Events, "When Flag Clicked", 40, 60));
+    myBlocks.push_back(Block(BlockType::Motion, "Move 10 Steps", 40, 120));
+    myBlocks.push_back(Block(BlockType::Looks, "Say Hello", 40, 180));
+    myBlocks.push_back(Block(BlockType::Control, "Wait 1 Sec", 40, 240));
 
     bool isRunning = true;
     SDL_Event event;
 
     while (isRunning) {
         while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
-                isRunning = false;
-            }
+            if (event.type == SDL_QUIT) isRunning = false;
         }
 
         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
         SDL_RenderClear(renderer);
 
         SDL_Rect sidebar = {0, 0, 250, 768};
-        SDL_SetRenderDrawColor(renderer, 230, 230, 230, 255);
+        SDL_SetRenderDrawColor(renderer, 240, 240, 240, 255);
         SDL_RenderFillRect(renderer, &sidebar);
+
+        SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255);
+        SDL_RenderDrawLine(renderer, 250, 0, 250, 768);
 
         for (auto& block : myBlocks) {
             block.render(renderer);
@@ -51,6 +43,5 @@ int main(int argc, char* argv[]) {
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
-
     return 0;
 }
