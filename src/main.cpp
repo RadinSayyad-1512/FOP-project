92aa131c694ea9cc83e7b49e93ca83cf6de57566
Radin <raden.sayyad.1512@gmail.com>
2026-02-17 12:24:42 +0330

    First Build of Drag&Drop

--- src/main.cpp
+++ src/main.cpp
@@ -5,22 +5,52 @@
 
 int main(int argc, char* argv[]) {
     SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window* window = SDL_CreateWindow("Sharif Scratch - Phase 1", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1024, 768, SDL_WINDOW_SHOWN);
+    SDL_Window* window = SDL_CreateWindow("FOP Project", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                                        1024, 768, SDL_WINDOW_SHOWN);
 
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
 
     std::vector<Block> myBlocks;
-    myBlocks.push_back(Block(BlockType::Events, "When Flag Clicked", 40, 60));
-    myBlocks.push_back(Block(BlockType::Motion, "Move 10 Steps", 40, 120));
-    myBlocks.push_back(Block(BlockType::Looks, "Say Hello", 40, 180));
-    myBlocks.push_back(Block(BlockType::Control, "Wait 1 Sec", 40, 240));
+    myBlocks.push_back(Block(BlockType::Events, "When Flag Clicked", 40, 50));
+    myBlocks.push_back(Block(BlockType::Motion, "Move 10 Steps", 40, 110));
+    myBlocks.push_back(Block(BlockType::Looks, "Say Hello", 40, 170));
+    myBlocks.push_back(Block(BlockType::Control, "Wait 1 Sec", 40, 230));
+
+    Block* draggedBlock = nullptr;
+    float offsetX = 0;
+    float offsetY = 0;
 
     bool isRunning = true;
     SDL_Event event;
 
     while (isRunning) {
         while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) isRunning = false;
+            if (event.type == SDL_QUIT)
+                isRunning = false;
+
+            if (event.type == SDL_MOUSEBUTTONDOWN) {
+                if (event.button.button == SDL_BUTTON_LEFT) {
+                    int mouseX = event.button.x;
+                    int mouseY = event.button.y;
+
+                    for (auto it = myBlocks.rbegin(); it != myBlocks.rend(); it++) {
+                        Block& block = *it;
+                        if (mouseX >= block.x && mouseX <= block.x + block.w &&
+                            mouseY >= block.y && mouseY <= block.y + block.h) {
+                            draggedBlock = &block;
+                            offsetX = mouseX - block.x;
+                            offsetY = mouseY - block.y;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            if (event.type == SDL_MOUSEBUTTONUP) {
+                if (event.button.button == SDL_BUTTON_LEFT) {
+                    draggedBlock = nullptr;
+                }
+            }
         }
 
         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
